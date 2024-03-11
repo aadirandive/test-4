0bb38d6ec432aca94f59e7c299ea19e06c63519b
mithilesh-kulkarni <137891270+mithilesh-kulkarni@users.noreply.github.com>
2024-03-11 16:29:39 +0530

    Create auto_deploy.sh

---
+++ auto_deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Set your Personal Access Token as an environment variable
+export GIT_TOKEN="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"
+
+# Step 1: Go to the specific folder
+cd /var/lib/jenkins/workspace/test
+
+# Step 2: Git pull using Personal Access Token (PAT)
+#git pull https://$GITHUB_PAT@github.com/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/https://github.com/skeletos-it/kairavistudios.git
+git config --global credential.helper '!f() { echo username=\$1; echo password=$GIT_TOKEN; }; f'
+
+git pull
+
+# Step 3: Install Node.js dependencies
+npm i
+
+# Step 4: Build the project
+npm run build
+
+# Step 5: Restart the application with PM2 (assuming PM2 is already installed)
+pm2 restart 1
+
+# Step 6: Verify PM2 status for process with ID 1
+pm2 status 1 | grep "online" && echo "Application with PM2 ID 1 is online" || echo "Application with PM2 ID 1 is not online"
